289277d2e387c0ae0720dd0b43d9ba19b5de9aee
Joeri Hermans <joeri@joerihermans.com>
2016-05-09 12:06:31 +0200

    Prepare stubs for outlier detection.

--- src/outlier_detection.py
+++ src/outlier_detection.py
@@ -3,8 +3,17 @@
 import os
 import sys
 
-def main():
-    print("STUB")
+def detect_outliers(output_directory):
+    print(output_directory)
 
-def __main__():
+def main():
+    num_arguments = len(sys.argv)
+    if(num_arguments <= 1):
+        print("Please specify the output directory.")
+    elif(num_arguments == 2 and is_dir(sys.argv[1])):
+        detect_outliers(sys.argv[1])
+    else:
+        print("Please specify an existing directory.")
+
+if(__name__ == '__main__'):
     main()
